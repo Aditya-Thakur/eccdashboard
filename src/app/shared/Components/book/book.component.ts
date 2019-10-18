e2dd33cae86a1a9405ccad093e7948a22fe4dbfe
Aditya Thakur <adityathakur532@gmail.com>
2019-10-18 14:04:20 +0530

    updated booking component

--- src/app/shared/Components/book/book.component.ts
+++ src/app/shared/Components/book/book.component.ts
@@ -1,35 +1,70 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { DataService } from '../../Services/data.service';
+import { BookService } from '../../Services/book.service';
+
+export interface ECC {
+  eccNo: number;
+  noOfRooms: number;
+}
 
 @Component({
-  selector: 'app-book',
-  templateUrl: './book.component.html',
-  styleUrls: ['./book.component.css']
+  selector: 'app-book',
+  templateUrl: './book.component.html',
+  styleUrls: ['./book.component.css']
 })
-export class BookComponent implements OnInit {
+export class BookComponent implements OnInit {
 
-  showBookingForm = true;
-  error;
-  bookForm;
-  constructor() {
-    this.bookForm = new FormGroup({
-      eccNo: new FormControl('',
-      [
-        Validators.required
-      ]
-      ),
-      roomNo: new FormControl('', [
-        Validators.required
-      ]),
-      employeeId: new FormControl('',
-      [
-        Validators.required
-      ]
-      )
-    });
-  }
+  listOfEcc = [];
+  showBookingForm = true;
+  error;
+  bookForm;
+  msg;
+  availableNoOfRooms = 0;
+  selectedEccCapacity;
+  constructor(private dataService: DataService, private bookService: BookService) {
+    this.bookForm = new FormGroup({
+      eccNo: new FormControl('',
+      [
+        Validators.required
+      ]
+      ),
+      roomNo: new FormControl('', [
+        Validators.required
+      ]),
+      employeeId: new FormControl('',
+      [
+        Validators.required
+      ]
+      )
+    });
+  }
 
-  ngOnInit() {
-  }
+  async ngOnInit() {
+     this.dataService.getEccRecord().subscribe( (res: ECC[]) => {
+      this.listOfEcc = res;
+    });
+  }
+
+ async getRoom(eccNo) {
+    this.availableNoOfRooms = this.listOfEcc.find( x => {
+      // tslint:disable-next-line: triple-equals
+      return x.eccNo == eccNo;
+    }).noOfRooms;
+    this.selectedEccCapacity = await Array.from({length: this.availableNoOfRooms}).map((x, i) => i);
+    this.selectedEccCapacity.push(this.availableNoOfRooms);
+    this.selectedEccCapacity.shift(0);
+    console.log(this.selectedEccCapacity);
+  }
+
+book() {
+  this.bookService.book(this.bookForm.value).subscribe(
+    (res) => {
+      this.msg = res;
+    }
+  );
+}
+
 
 }
+
--- src/app/shared/Services/book.service.ts
+++ src/app/shared/Services/book.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { catchError, map } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  book(data) {
+    return this.http.post(`http://localhost:3000/book/bookNewRoom`, data).pipe(
+      map((res) => {
+        return res;
+      }),
+      catchError(this.handleError));
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    console.log(error);
+    // return an observable with a user friendly message
+    return throwError('Error! something went wrong.');
+  }
 }
